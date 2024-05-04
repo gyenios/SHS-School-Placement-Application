1c2438f20124f2e31be28454be1bdd2d1ca7c991
gyenios <98366207+gyenios@users.noreply.github.com>
2024-05-04 15:04:40 +0100

    Update sp_functions.py

--- sp_functions.py
+++ sp_functions.py
@@ -1,159 +1,141 @@
-from random import choice
-import string
-
-def get_voucher():
-    #Generating pin
-    x = range(100000000)
-    pin = choice(x)
-    
-    #Generating serial number
-    characters = string.ascii_letters + string.digits  # Letters and numbers
-    serial_number = ''.join(choice(characters) for _ in range(10))
-    print(f'''
-    \t\t-----VOUCHER DETAILS-----
-    \t\tSERIAL NUMBER: {serial_number}
-    \t\tPIN: {pin}
-    ''')
-
-import csv
-# Define a function to read the CSV file and filter schools
-def filter_schools(csv_file, option, region=None, gender=None, category=None):
-    filtered_schools, codes = [],[]
-
-    with open(csv_file, newline='') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            if option == 1:
-                if row['Region'] == region:
-                    filtered_schools.append(row['School Name'])
-                    codes.append(row['Code'])
-            elif option == 2:
-                if row['Gender'] == gender:
-                    filtered_schools.append(row['School Name'])
-                    codes.append(row['Code'])
-            elif option == 3:
-                if row['Category'] == category:
-                    filtered_schools.append(row['School Name'])
-                    codes.append(row['Code'])
-            elif option == 4:
-                if row['Region'] == region and row['Gender'] == gender and row['Category'] == category:
-                    filtered_schools.append(row['School Name'])
-                    codes.append(row['Code'])
-            filtered = [filtered_schools,codes]
-    return filtered
-
-def search_schools():
+PassWord = 'gyenios'
+def get_password():
+    def authenticate(password):
+        if password == PassWord:
+            print('--Login successful--')
+            return True
+        else:
+            print('Wrong password')
+            return False
+        
+    login = True
+    tries = 3
     while True:
-        Schools = []
-        print('''
-        Search by:
-        [1] Region
-        [2] Gender
-        [3] Category
-        [4] All
-        ''')
-        choice = int(input())
-        if choice == 1:
-            region = input("Region: ").title()
-            Schools = filter_schools("schools.csv", int(choice), region=region)
-        elif choice == 2:
-            gender = input("Gender: ").title()
-            Schools = filter_schools("schools.csv", int(choice), gender=gender)
-        elif choice == 3:
-            category = input("Category: ").title()
-            Schools = filter_schools("schools.csv", int(choice), category=category)
-        elif choice == 4:
-            region = input("Region: ").title()
-            gender = input("Gender: ").title()
-            category = input("Category: ").title()
-            Schools = filter_schools("schools.csv", int(choice), region=region, gender=gender, category=category)
-        print("\n")
-        schools = Schools[0]
-        codes = Schools[1]
-        if len(schools) > 0:
-            print("SCHOOL","SCHOOL CODE",sep = "\t\t\t\t\t\t\t")
-            for i in range(len(schools)):
-                print(f"{schools[i]:<60}",codes[i])
+        if tries == 0:
+            print('You entered the wrong password too many times')
             break
         else:
-            print('No school matching search')
+            password = input('Enter Admin password: ')
+            login = authenticate(password)
+            if login == True:
+                break
+            else:
+                tries -= 1
+    return login
 
-# Get candidate information
-def personal_info():
-    name = input("Full name: ").title()
-    school = input("School: ").title()
-    year = int(input("Year of Exam: "))
-    index = input('Index number: ')
-    return [name,year,school,index]
-
-# Take BECE results input
-def BECE_results(name,year,school):
-    
-    # The subjects list contains two sub-lists, the first is for core subjects and the second is for electives
-    subjects = [['ENGLISH LANGUAGE', 'SOCIAL STUDIES', 'INTEGRATED SCIENCE', 'MATHS'],['ICT', 'FRENCH', 'GA','TWI','PRE-TECH','HOME ECONS']]
-
-    # Get details of candidate's electives
-    def get_electives():
-        electives = []
-        n = 1
-        print("ELECTIVES")
-        for e in subjects[1]:
-            print(f"{n}.",e, sep=" ")
-            n += 1
-
-        select = input("Select the electives you wrote (Example: 1,2,4,5) ")
-        choice = [1,2,3,4,5,6]
-        select = select.replace(',','') # Stripping off commas
-
-        # Store selected electives in subject[1]
-        indices = []
-        for i in select:
-            a = int(i)-1
-            indices.append(a)
-        for e in indices:
-            electives.append(subjects[1][e])
-        subjects[1] = electives
+class Admin:
+    def __init__(self):
+        pass
         
-    # Get candidate's BECE results
-    def results():
-        grades = []
-        print("\n\nSUBJECT\t\t\t       GRADE")
-        for e in subjects[0]:
-            print(f"{e:<20}", end = "\t\t")
-            grades.append(int(input()))
-        for e in subjects[1]:
-            print(f"{e:<20}", end = "\t\t")
-            grades.append(int(input()))
-        return grades
+    def menu(self):
+        print('''
+                [1] View sales
+                [2] View Inventory
+                [3] Update inventory
+                [4] Exit
+            ''')
+
+class Customer:
+    def __init__(self):
+        pass
+
+    def menu(self):
+        print('''
+                [1] Search item
+                [2] Purchase item
+                [3] View Cart
+                [4] Go to checkout
+                [5] Exit
+            ''')
     
-    print(f'''
-        Name: {name}
-        Examination Year: {year}
-        School: {school}
-         ''')
+def search():
+    import csv
+    # Define a function to read the CSV file and filter schools
+    def filter_products(csv_file, option, Brand=None, Model=None, Unit_price=None):
+        items, brands, models, unit_prices = [], [], [], []
+        
+        with open(csv_file, newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if option == 1:
+                    if row['Brand'] == Brand:
+                        items.append(row['Item'])
+                        brands.append(row['Brand'])
+                        models.append(row['Model'])
+                        unit_prices.append(row['Unit Price(USD)'])
+                elif option == 2:
+                    if row['Model'] == Model:
+                        items.append(row['Item'])
+                        brands.append(row['Brand'])
+                        models.append(row['Model'])
+                        unit_prices.append(row['Unit Price(USD)'])
+                elif option == 3:
+                    if Unit_price == '1':
+                        x, y = 0.00, 99.99
+                    elif Unit_price == '2':
+                        x, y = 100.00, 299.99
+                    elif Unit_price == '3':
+                        x, y = 300.00, 499.99
+                    elif Unit_price == '4':
+                        x, y = 500.00, 799.99
+                    elif Unit_price == '5':
+                        x, y = 800.00, 2000.00
+                    price = float(row['Unit Price(USD)'])
+                    if x <= price <= y:
+                        items.append(row['Item'])
+                        brands.append(row['Brand'])
+                        models.append(row['Model'])
+                        unit_prices.append(row['Unit Price(USD)'])
+                elif option == 4:
+                    if row['Brand'] == Brand and row['Model'] == Model:
+                        items.append(row['Item'])
+                        brands.append(row['Brand'])
+                        models.append(row['Model'])
+                        unit_prices.append(row['Unit Price(USD)'])
 
-    get_electives() # Calling the function to take details of electives
-    grades = results() # Calling the function to take details of BECE results 
-    return grades
+    results = [items, brands, models, unit_prices]
+    return results
 
-def get_aggregate(grades):
-    aggregate = 0
-    for e in grades[:4]: # Iterating through grades for core subjects
-        aggregate += e
+    choice = '1'
+    while choice == '1':
+        Results = []
+        print('''
+        Search by:
+        [1] Brand
+        [2] Model
+        [3] Price range
+        [4] All
+        ''')
+        choice = input()
+        if choice == '1':
+            brand = input("Brand: ").title()
+            Results = filter_products("inventory.csv", int(choice), Brand=brand)
+        elif choice == '2':
+            model = input("Model: ").title()
+            Results = filter_products("inventory.csv", int(choice), Model=model)
+        elif choice == '3':
+            print('''
+                    --PRICE RANGES--
+                    [1] 0.00 - 99.99
+                    [2] 100.00 - 299.99
+                    [3] 300.00 - 499.99
+                    [4] 500.00 - 799.99
+                    [5] 800.00 - 2000.00
+                    ''')
+            price = input("Price range: ").title()
+            Results = filter_products("inventory.csv", int(choice), Unit_price=price)
+        elif choice == '4':
+            brand = input("Brand: ").title()
+            model = input("Model: ").title()
+            Results = filter_products("inventory.csv", int(choice), Brand=brand, model=model)
+        print("\n")
+        items = Results[0]
+        brands = Results[1]
+        models = Results[2]
+        unit_prices = Results[3]
+        print("ITEMS\t\t\t\tBRANDS\t\t\t\tMODELS\t\t\t\tUNIT PRICE")
+        for i in range(len(items)):
+            print(f"{items[i]:<40} {brands[i]:<40} {models[i]:<40}",unit_prices[i])
+        
 
-    electives = grades[4:]
-    
-    # Arranging grades in ascending order
-    i = 1
-    while i < len(electives):
-        if electives[i-1] > electives[i]:
-            electives[i],electives[i-1] = electives[i-1], electives[i] # Swapping
-        i += 1
-    print(electives)
-    aggregate = aggregate + electives[0] + electives[1]
-    return aggregate
-
-name,year,school,index = personal_info()        
-grades = BECE_results(name,year,school)
-aggregate = get_aggregate(grades)
-print(f'Aggregate: {aggregate:02}')
+        
