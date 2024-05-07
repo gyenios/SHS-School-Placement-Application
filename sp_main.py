531089a2deb906a8cbaca3b4b3e2fd690d3bf179
gyenios <98366207+gyenios@users.noreply.github.com>
2024-05-08 00:08:50 +0100

    Update sp_main.py

--- sp_main.py
+++ sp_main.py
@@ -1,5 +1,3 @@
-from sp_functions import * 
-name = input("Full name: ")
 '''
 Divided into 3 sessions
 [1] Personal information and BECE results input
@@ -17,3 +15,44 @@ At school selection, students can select 4 schools, 4 courses and 4 statuses (B 
 [3] School placement
 Based on the aggregate, the student is placed. 
 '''
+
+from sp_functions import * 
+
+def main():
+    # 1. Getting Personal info and BECE results
+    name,year,school,index = personal_info()
+    grades = BECE_results(name,year,school)
+    aggregate = get_aggregate(grades)
+
+    # 2. Voucher generation, validation and school selection
+    serial_number,pin = get_voucher()
+    def menu():
+        input('Press any key to proceed to school selection')
+        print('SCHOOL SELECTION PORTAL')
+        option = int(input('Press 1 to search school(s) or any other key to proceed with school selection'))
+        try:
+            if option == 1:
+                while True:
+                    print('''
+                    What would you like to do?
+                    [1] Perform another search
+                    [2] Continue to school selection
+                    ''')
+                    option = int(input('>> '))
+                    try:
+                        if option == 1:
+                            continue
+                        elif option == 2:
+                            break
+                        else:
+                            print('Enter 1 or 2')
+                selected = school_selection()
+                    except ValueError:
+                        print('Enter 1 or 2')
+            else:
+                selected = school_selection()    
+        except ValueError:
+            selected = school_selection()
+        return selected
+        
+    # 3. School placement
