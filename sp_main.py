135ebb15239643e95294bffafce994c3fdb1e6c8
gyenios <98366207+gyenios@users.noreply.github.com>
2024-05-07 21:35:11 +0100

    Update sp_main.py

--- sp_main.py
+++ sp_main.py
@@ -1,5 +1,19 @@
 from sp_functions import * 
 name = input("Full name: ")
 '''
+Divided into 3 sessions
+[1] Personal information and BECE results input
+At this part, candidates enter their name, index and school details
+They also enter their BECE results and the aggregate is calculated
 
+[2] Voucher generation and school selection
+Get voucher before proceeding to school selection
+At school selection, candidates should enter voucher details
+Voucher details validation system [3 tries only]
+When voucher is valid proceed to school selection [option to search for schools]
+At school selection, students can select 4 schools, 4 courses and 4 statuses (B or D)
+
+
+[3] School placement
+Based on the aggregate, the student is placed. 
 '''
